fe1f137a5d066edca5499f12d608344a52aa2549
walk8243 <walk8243@gmail.com>
2021-03-10 02:24:18 +0900

    リクエストからパラメータを受け取って検索する

--- src/main/java/xyz/walk8243/nogiprofile/controller/MemberInfoController.java
+++ src/main/java/xyz/walk8243/nogiprofile/controller/MemberInfoController.java
@@ -3,7 +3,9 @@ package xyz.walk8243.nogiprofile.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import xyz.walk8243.nogiprofile.model.MemberInfo;
@@ -18,5 +20,15 @@ public class MemberInfoController {
 	@GetMapping(path = "/all")
 	public @ResponseBody Iterable<MemberInfo> getAllMembers() {
 		return memberInfoRepository.findAll();
+	}
+
+	@GetMapping(path = "/class")
+	public @ResponseBody Iterable<MemberInfo> getMembersByClassForQuery(@RequestParam("value") Integer memberClass) {
+		return memberInfoRepository.findByMemberClass(memberClass);
+	}
+
+	@GetMapping(path = "/class/{value}")
+	public @ResponseBody Iterable<MemberInfo> getMembersByClassForPath(@PathVariable("value") Integer memberClass) {
+		return memberInfoRepository.findByMemberClass(memberClass);
 	}
 }
--- src/main/java/xyz/walk8243/nogiprofile/repository/MemberInfoRepository.java
+++ src/main/java/xyz/walk8243/nogiprofile/repository/MemberInfoRepository.java
@@ -1,9 +1,11 @@
 package xyz.walk8243.nogiprofile.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import xyz.walk8243.nogiprofile.model.MemberInfo;
 
 public interface MemberInfoRepository extends CrudRepository<MemberInfo, Integer> {
-
+	public List<MemberInfo> findByMemberClass(Integer memberClass);
 }

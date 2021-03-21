dfb544dc30190774d26a4a8b0a671b5d83928a6d
walk8243 <walk8243@gmail.com>
2021-03-22 01:44:59 +0900

    起動アプリケーションクラスを修正

--- src/main/java/xyz/walk8243/nogiprofile/api/NogiProfileApiApplication.java
+++ src/main/java/xyz/walk8243/nogiprofile/api/NogiProfileApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NogiProfileApplication {
+public class NogiProfileApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NogiProfileApplication.class, args);

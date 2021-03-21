2a4dcc02dda31cdc808a1c51a19f498f211be073
walk8243 <walk8243@gmail.com>
2021-03-22 02:18:58 +0900

    バグ修正

--- src/main/java/xyz/walk8243/nogiprofile/api/NogiProfileApiApplication.java
+++ src/main/java/xyz/walk8243/nogiprofile/api/NogiProfileApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NogiProfileApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NogiProfileApplication.class, args);
+		SpringApplication.run(NogiProfileApiApplication.class, args);
 	}
 
 }

ccdc35495412dca36f28978b50710254b392bb23
walk8243 <walk8243@gmail.com>
2021-04-03 23:16:22 +0900

    MemberとMemberInfoをAPIとしては統合

--- src/main/java/xyz/walk8243/nogiprofile/api/controller/MemberController.java
+++ src/main/java/xyz/walk8243/nogiprofile/api/controller/MemberController.java
@@ -3,20 +3,32 @@ package xyz.walk8243.nogiprofile.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import xyz.walk8243.nogiprofile.api.model.Member;
-import xyz.walk8243.nogiprofile.api.repository.MemberRepository;
+import xyz.walk8243.nogiprofile.api.model.MemberInfo;
+import xyz.walk8243.nogiprofile.api.repository.MemberInfoRepository;
 
 @Controller
 @RequestMapping(path = "/member")
 public class MemberController {
 	@Autowired
-	private MemberRepository memberRepository;
+	private MemberInfoRepository memberInfoRepository;
 
-	@GetMapping(path = "/all")
-	public @ResponseBody Iterable<Member> getAllMembers() {
-		return memberRepository.findAll();
+	@GetMapping(path = {"", "/", "/all"})
+	public @ResponseBody Iterable<MemberInfo> getAllMembers() {
+		return memberInfoRepository.findAll();
+	}
+
+	@GetMapping(path = "/class")
+	public @ResponseBody Iterable<MemberInfo> getMembersByClassForQuery(@RequestParam("value") Integer memberClass) {
+		return memberInfoRepository.findByMemberClass(memberClass);
+	}
+
+	@GetMapping(path = "/class/{value}")
+	public @ResponseBody Iterable<MemberInfo> getMembersByClassForPath(@PathVariable("value") Integer memberClass) {
+		return memberInfoRepository.findByMemberClass(memberClass);
 	}
 }

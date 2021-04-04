c27eea44d300849876c8bbf8c0cf46bf2b3f5779
walk8243 <walk8243@gmail.com>
2021-04-05 02:14:33 +0900

    外部にパスワードの問い合わせができる状態に

--- src/main/java/xyz/walk8243/nogiprofile/api/AppProperties.java
+++ src/main/java/xyz/walk8243/nogiprofile/api/AppProperties.java
@@ -1,7 +1,9 @@
 package xyz.walk8243.nogiprofile.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,17 +11,49 @@ import lombok.Setter;
 @Component
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
+	@Autowired
+	RestTemplate restTemplate;
+
 	@Getter @Setter private String name = "nogiprofile-api";
 	@Getter private MysqlProperties mysql = new MysqlProperties();
+	@Getter private KeyStoreProperties keystore = new KeyStoreProperties();
 
 	public class MysqlProperties {
 		@Getter @Setter private String host;
 		@Getter @Setter private String database;
 		@Getter @Setter private String username;
-		@Getter @Setter private String password;
+		@Getter private String password;
+		@Getter @Setter private KeyStoreConfig passkey = new KeyStoreConfig();
+
+		public void setPassword(String password) {
+			if(password.isEmpty()) {
+				if(passkey.validate()) {
+					this.password = restTemplate.getForObject(keystore.getUrl() + "?group=" + passkey.getGroup() + "&key=" + passkey.getKey(), String.class);
+				}
+			} else {
+				this.password = password;
+			}
+		}
 
 		public String toLogStr() {
 			return "{host: " + host + ", database: " + database + ", username: " + username + "}";
 		}
+	}
+
+	public class KeyStoreConfig {
+		@Getter @Setter private String group;
+		@Getter @Setter private String key;
+
+		public boolean validate() {
+			if(group.isEmpty() || key.isEmpty()) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
+
+	public class KeyStoreProperties {
+		@Getter @Setter private String url;
 	}
 }
--- src/main/java/xyz/walk8243/nogiprofile/api/BeanConfiguration.java
+++ src/main/java/xyz/walk8243/nogiprofile/api/BeanConfiguration.java
@@ -1,9 +1,15 @@
 package xyz.walk8243.nogiprofile.api;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableConfigurationProperties(AppProperties.class)
 public class BeanConfiguration {
+	@Bean
+	RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
